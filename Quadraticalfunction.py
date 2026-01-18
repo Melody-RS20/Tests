faf1e1df855c8b868690f929b9eadd4ce497705c
Melody-RS20 <sagilak8@gmail.com>
2026-01-18 14:56:03 +0100

    quadratic function

---
+++ Quadraticalfunction.py
@@ -0,0 +1,22 @@
+# f(x) = ax^2 + bx + c
+
+function_task = "Task: give the value for x, a, b, and c "
+print(function_task)
+x = float  (input("Enter a value for x:"))
+user_input_a = float (input( "Enter the value for a:"))
+
+user_input_b = float (input( "Enter the value for b:"))
+
+user_input_c = float (input( "Enter the value for c:"))
+
+
+
+
+
+function_input = (user_input_a)*x*x + (user_input_b)*x + (user_input_c)
+print(function_input)
+#print("f(x)=", function_input)
+
+
+#function= f(x)= ax**2 + bx + c 
+#User_input = "a = user_ input() "
